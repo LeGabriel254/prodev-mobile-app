14a5ddcd549ace53aa59562f93ddaa358d56f53b
Leon Gabriel <g73346920@gmail.com>
2025-02-20 13:00:17 +0300

    modified

--- prodev-mobile-app-0x05/babel.config.js
+++ prodev-mobile-app-0x05/babel.config.js
@@ -1,8 +1,4 @@
-const { default: plugin } = require("tailwindcss");
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel']
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['tailwindcss-native/babel'],
 };

7c3da1276f27957dabf08f557e02d45160edc0b8
Leon Gabriel <g73346920@gmail.com>
2025-02-21 14:56:46 +0300

    Initial commits

---
+++ prodev-mobile-app-0x05/app/Sign.tsx
@@ -0,0 +1,105 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/signinStyles";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+
+export default function Index() {
+  const router = useRouter()
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* arrow-back */}
+        <View style={styles.navGroup}>
+          <Ionicons name="arrow-back" size={25} onPress={() => router.push("/")}/>
+          <Image source={require('@/assets/images/logo-green.png')} />
+        </View>
+
+        {/* Instruction */}
+        <Text style={styles.largeText}>Sign in to your</Text>
+        <Text style={styles.largeText}>Account</Text>
+        <Text style={styles.smallText}>
+          Enter your email and password to sign in.
+        </Text>
+
+        {/* Form Group */}
+        <View style={styles.formGroup}>
+          {/* Email */}
+          <View>
+            <Text style={styles.placeholderText}>Email</Text>
+            <TextInput keyboardType="email-address" style={styles.inputField} />
+          </View>
+          {/* Password */}
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Password</Text>
+            <View style={styles.passwordGroup}>
+              <TextInput style={{ flex: 1 }} />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+          {/* Forgot Password */}
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </View>
+
+        {/* Signing in Buttons */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View
+            style={styles.divider}
+          ></View>
+          <Text
+            style={styles.dividerText}
+          >
+            OR
+          </Text>
+          <View
+            style={styles.divider}
+          ></View>
+        </View>
+
+        {/* Authentication Options */}
+        <View style={styles.socialMediaButtonGroup}>
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={require("@/assets/images/google.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Image source={require("@/assets/images/facebook.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        
+        {/* New users */}
+        <View style={styles.subTextGroup}>
+          <Text style={styles.subText}>Don't have an account?</Text>
+          <Text style={styles.subTextJoin} onPress={() => router.push("/join")}>Join now</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
---
+++ prodev-mobile-app-0x05/app/index.tsx
@@ -0,0 +1,113 @@
+import { Text, View, StyleSheet, Image, ImageBackground, Dimensions, TouchableOpacity } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { router, useRouter } from "expo-router";
+import React from "react";
+
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    // entire app wrapped inside SafeAreaProvider and SafeAreaView to prevent UI overlap with device notches.
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+
+        {/* App BackgroundImage */}
+        <ImageBackground
+          source={require("@/assets/images/hero-icon.png")}
+          style={styles.background}
+          resizeMode="cover"
+        >
+          {/* Company Logo */}
+          <View style={styles.container}>
+            <View style={styles.companyLogo}>
+              <Image source={require("@/assets/images/Logo.png")} />
+            </View>
+
+            {/* Hero Section */}
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
+
+            {/* Button section */}
+            <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={{ ...styles.textSmall, color: "black" }} onPress={() => router.push("/join")}>Join here</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.transparentButton}>
+                  <Text style={styles.textSmall} onPress={() => router.push("/Sign")}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }} onPress={() => router.push("/(home)")}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    width: "100%",
+    height: Dimensions.get("window").height,
+  },
+  companyLogo: {
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+    marginBottom: 50,
+  },
+  textGroup: {
+    alignItems: "center",
+  },
+  textLarge: {
+    color: "white",
+    fontWeight: "800",
+    fontSize: 40,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  textSmall: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "200",
+    textAlign: "center",
+  },
+  transparentButton: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    flex: 1,
+  },
+  button: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    backgroundColor: "white",
+    flex: 1,
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    gap: 20,
+    paddingHorizontal: 20,
+  },
+});
